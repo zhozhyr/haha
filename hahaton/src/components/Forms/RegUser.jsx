8a2a48953d04eab5df67688becb578a5f3369254
Aleksandra255685 <sashaven2003@mail.ru>
2024-09-12 20:26:53 +0700

    registration 2.0

---
+++ hahaton/src/components/Forms/RegUser.jsx
@@ -0,0 +1,20 @@
+import './RegForm.css'
+
+function UserForm() {
+    return ( 
+        <form>
+            <label htmlFor='input1'>Поле 1</label>
+            <input id='input1' name='input1' type='text' className='styled-input'/>
+
+            <label htmlFor='input2'>Поле 2</label>
+            <input id='input2' name='input2' type='text' className='styled-input'/>
+
+            <label htmlFor='input3'>Поле 3</label>
+            <input id='input3' name='input3' type='text' className='styled-input'/>
+
+            <button className='styled-button'>Зарегистрироваться</button>
+        </form>
+    );
+}
+
+export default UserForm;
--- hahaton/src/pages/Reg/Reg.jsx
+++ hahaton/src/pages/Reg/Reg.jsx
@@ -1,15 +1,35 @@
-import './Reg.css'
+import { Tab, TabGroup, TabList, TabPanel, TabPanels } from '@headlessui/react'
+import { useState } from "react";
 
+import UserForm from '../../components/Forms/RegUser';
+import OrganizationForm from '../../components/Forms/RegOrganization';
+
+import './Reg.css'
 import bg from '../../assets/bg_reg.png'
 
 function Registration() {
+
+    const [toggleState, setToggleState] = useState(1);
+    const toggleTab = (index) => {
+        setToggleState(index);
+    };
+
     return ( 
         <>
             <h1 className="header-title">BNTPNHA API</h1>
             <img src={bg} alt='bg'className='bg'/> 
             <div className='registration'>
                 <h2 className="registration-title">РЕГИСТРАЦИЯ</h2>
-
+                <TabGroup >
+                    <TabList className="tab_list">
+                        <Tab as='div' className={toggleState === 1 ? "tabs active-tabs" : "tabs"} onClick={() => toggleTab(1)}>Пользователь</Tab>
+                        <Tab as='div' className={toggleState === 2 ? "tabs active-tabs" : "tabs"} onClick={() => toggleTab(2)}>Организация</Tab>
+                    </TabList>
+                    <TabPanels>
+                        <TabPanel><UserForm/></TabPanel>
+                        <TabPanel><OrganizationForm/></TabPanel>
+                    </TabPanels>
+                </TabGroup>
             </div>
             
 
